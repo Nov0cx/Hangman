7bbeaf02c7131715a8a0fdc5272bb5c922e6ac85
Nov0cx <niklas.leyraf44@gmail.com>
2020-11-11 17:48:55 +0100

    v1.1_Snapshot

--- src/main/java/com/nov0cx/Hangman.java
+++ src/main/java/com/nov0cx/Hangman.java
@@ -6,8 +6,10 @@ import lombok.Setter;
 import lombok.SneakyThrows;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -18,30 +20,39 @@ public class Hangman {
     @SneakyThrows
     public Hangman() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] strings = new String[]{
-                "Apfel",
-                "Banane",
-                "Zitrone",
-                "Limette",
-                "Wassermelone",
-                "Drachenfrucht",
-                "Blauebeere",
-                "Himmbeere",
-                "Johannisbeere",
-                "Erdbeere",
-                "Kirsche",
-                "Badewanne",
-                "Fahrt",
-                "Protokolle",
-                "Drehen",
-                "Prophylaktisch",
-                "Karikatur"
-        };
-        String c = strings[new Random().nextInt(strings.length)].toLowerCase();
-        System.out.println(ConsoleColor.GREEN + "The possibilities are: " + Arrays.toString(strings) + ConsoleColor.RESET);
+
+        //looking for the path of the text-file with the list
+        String path = "C:/Users/nikla/Desktop/Hangman/depend/list.txt";
+        System.out.println("Write the path to the list and after that 'next'");
+        String line;
+        while (true) {
+            line = reader.readLine();
+            if(line.equals("next"))
+                break;
+            path = (line);
+        }
+
+        //adding the list of the file in the list
+        File file = new File(path);
+        ArrayList<String> strings = new ArrayList<>();
+        FileReader reader1 = new FileReader(file);
+        BufferedReader reader2 = new BufferedReader(reader1);
+        String l;
+        while ((l = reader2.readLine()) != null) {
+            strings.add(l);
+        }
+
+        //start of the game
+        String c = strings.get(new Random().nextInt(strings.size() + 1)).toLowerCase();
+        System.out.println(ConsoleColor.GREEN + "The possibilities are: " + Arrays.toString(strings.toArray()) + ConsoleColor.RESET);
         System.out.println(ConsoleColor.GREEN + "The word is " + c.toCharArray().length + " characters long." + ConsoleColor.RESET);
+
+        //the number of wrong guesses
         int wrong = 0;
+        //the maximum of wrong guesses
         int max = 10;
+
+        //game loop
         while (true) {
             String read = reader.readLine().toLowerCase();
             if (wrong > max)
@@ -55,7 +66,6 @@ public class Hangman {
                 System.out.println(ConsoleColor.RED + read
                         + " isn't part of the word. You have " + wrong
                         + " wrong guesses out of max " + max + " wrong guesses." + ConsoleColor.RESET);
-
             }
         }
         if (wrong > max)
